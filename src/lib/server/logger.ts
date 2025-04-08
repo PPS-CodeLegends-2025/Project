9e4602495360bfae77217e3011b6c8a132e70d5d
Daimonix <dimaprotsap@gmail.com>
2025-04-08 11:38:23 +0300

    Fixed build issues

--- src/lib/server/logger.ts
+++ src/lib/server/logger.ts
@@ -1,12 +1,8 @@
-import { dev } from '$app/environment';
 import pino from 'pino';
 
 export const logger = pino(
-	dev
-		? undefined
-		: pino.destination({
-				dest: `./codelegends-server-${new Date().toISOString()}.log`,
-				minLength: 4096,
-				sync: false
-			})
+	pino.destination({
+		minLength: 4096,
+		sync: false
+	})
 );
